6d5cf91b1bf8697b789e65e2b4d7888e6666b6a5
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2024-08-16 00:00:30 +0300

    Daha temiz Kod yazdım

--- main.js
+++ main.js
@@ -1,77 +1,53 @@
-const changeTeamName1 = document.getElementById('changeTeamName1');
-const TeamName1 = document.getElementById('Team1Name');
-const changeTeamName2 = document.getElementById('changeTeamName2');
-const TeamName2 = document.getElementById('Team2Name');
+function increaseScore(teamName) {
+  const scoreId = `TeamScore${teamName}`;
+  const scoreElement = document.getElementById(scoreId);
+  scoreElement.innerText++;
+}
 
-// Takım 1 İsim değiştirme Butonu
-changeTeamName1.addEventListener('click', e => {
-    let NewTeamName = prompt("Takım 1'in Yeni İsmini Giriniz");
-    if (NewTeamName === "") {
-        alert("Boş değer girilmez!!!");
-    } else {
-        TeamName1.innerHTML = NewTeamName;
+function toggleClassTemporarily(element, className, duration) {
+  element.classList.remove(className);
+  setTimeout(function () {
+    element.classList.add(className);
+  }, duration);
+}
+
+function decreaseScore(teamName) {
+  const scoreId = `TeamScore${teamName}`;
+  const scoreElement = document.getElementById(scoreId);
+  const scoreInteger = Number(scoreElement.innerText);
+  if (scoreInteger > 0) {
+    scoreElement.innerText--;
+  } else {
+    // Skor 0 iken azaltmaya çalışılırsa alert gösterilmesi
+    const myElement = document.getElementById("liveAlertPlaceholder");
+    toggleClassTemporarily(myElement, "d-none", 3000);
+  }
+}
+
+function setNamePrompt(teamName) {
+  const teamId = `Team${teamName}`;
+  const teamNameElement = document.getElementById(teamId);
+  const teamNamePrompt = prompt("Takımın Yeni İsmini giriniz");
+  teamNameElement.innerText = teamNamePrompt;
+}
+
+function setScorePrompt(teamName) {
+  const score = prompt("Takımın Skorunu Girin");
+  if (!isNaN(score) && score != null) {
+    const parseIntScore = Number(score);
+    if (Number.isInteger(parseIntScore) && parseIntScore >= 0) {
+      if (parseIntScore > 15) {
+        alert("Hoop Emmioglu Basketbol mu oynuyorsun O skor ne");
+      } else {
+        document.getElementById(`TeamScore${teamName}`).textContent =
+          parseIntScore;
+      }
     }
-});
+  }
+}
 
-// Takım 2 İsim değiştirme Butonu
-changeTeamName2.addEventListener('click', e => {
-    let newTeamName2 = prompt("Takım 2'nin Yeni İsmini Giriniz");
-    if (newTeamName2 === "") {
-        alert("Boş değer girilmez!!!");
-    } else {
-        TeamName2.innerHTML = newTeamName2;
-    }
-});
-
-// Takım 1 Skorunu Değiştirme
-const Team1NewScore = document.getElementById('team1-scoreincrease');
-const Team1ScoreReduction = document.getElementById('team1-scorereduction');
-const Team1ScoreCount = document.getElementById('team1ScoreCount');
-
-Team1NewScore.addEventListener('click', e => {
-    let Team1Score = prompt("Takım 1'in Skorunu Giriniz");
-
-    // Sayısal değeri kontrol et
-    if (Team1Score === "" || isNaN(Team1Score)) {
-        alert("Geçerli bir sayı giriniz!!!");
-    } else {
-        // Sayıya dönüştür ve güncelle
-        Team1ScoreCount.innerHTML = parseInt(Team1Score, 10);
-    }
-});
-
-// Takım 1 Skor Azaltma
-Team1ScoreReduction.addEventListener('click', e => {
-    let currentScore = parseInt(Team1ScoreCount.innerHTML, 10);
-    if (currentScore > 0) {
-        currentScore--;
-        Team1ScoreCount.innerHTML = currentScore;
-    }
-});
-
-// Takım 2 Skorunu Değiştirme
-const Team2ScoreIncrease = document.getElementById('team2-scoreincrease');
-const Team2ScoreCount = document.getElementById('team2ScoreCount');
-const Team2ScoreReduction = document.getElementById('team2-scorereduction');
-
-Team2ScoreIncrease.addEventListener('click', e => {
-    let Team2NewScore = prompt("Takım 2'nin Skorunu Giriniz");
-
-    // Sayısal değeri kontrol et
-    if (Team2NewScore === "" || isNaN(Team2NewScore)) {
-        alert("Geçerli bir sayı giriniz!!!");
-    } else {
-        // Sayıya dönüştür ve güncelle
-        Team2ScoreCount.innerHTML = parseInt(Team2NewScore, 10);
-    }
-});
-
-// Takım 2 Skor Azaltma
-Team2ScoreReduction.addEventListener('click', e => {
-    let team2Score = parseInt(Team2ScoreCount.innerHTML, 10);
-    // Negatif değeri kabul etmedik
-    if (team2Score > 0) {
-        team2Score--;
-        Team2ScoreCount.innerHTML = team2Score;
-    }
-});
+function teamScoreReset(teamName) {
+  const scoreReset = `TeamScore${teamName}`;
+  const scoreElement = document.getElementById(scoreReset);
+  scoreElement.textContent = "0";
+}
